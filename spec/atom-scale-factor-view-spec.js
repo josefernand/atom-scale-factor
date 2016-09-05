85cdab17aa52f4cb42b476c4f794edc012dc878a
Jose Fernandez <joseafern@gmail.com>
2016-09-05 16:12:37 -0300

    Initial commit

---
+++ spec/atom-scale-factor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomScaleFactorView from '../lib/atom-scale-factor-view';
+
+describe('AtomScaleFactorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
